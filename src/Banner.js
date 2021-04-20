46a74da1bec83bacf5ce20562d8aefee8614a130
HarisGkn <haris.gjo@gmail.com>
2021-04-20 10:48:14 +0300

    Created rows and styled them

---
+++ src/Banner.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    return (
+        <header>
+
+        </header>
+    )
+}
+
+export default Banner
--- src/Row.js
+++ src/Row.js
@@ -1,22 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
+import "./Row.css";
 
-function Row({ title, fetchUrl }) {
-const [movies, setMovies] = useState( [] );
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-    useEffect(()=> {
-        async function fetchData(){
+function Row({ title, fetchUrl, isLargeRow }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
             const request = await axios.get(fetchUrl);
             console.log(request);
+            setMovies(request.data.results);
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+
+    console.log(movies);
 
     return (
-        <div>
-            
+        <div className="row">
+
             <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img
+                        key={movie.id}
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        src={isLargeRow ? base_url + movie.poster_path : base_url + movie.backdrop_path}
+                        alt={movie.name}
+                    />
+                ))}
+            </div>
 
         </div>
     )
